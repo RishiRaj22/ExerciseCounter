7c15c22e865d3c84b85026f9251a2bd9fc84a6d4
Rishi Raj <rishiraj6686@gmail.com>
2017-07-06 19:42:45 +0530

    License changed

--- app/src/main/java/me/itsrishi/exercisecounter/ExerciseActivity.java
+++ app/src/main/java/me/itsrishi/exercisecounter/ExerciseActivity.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 4017 Rishi Raj
+ * MIT License
+ *
+ * Copyright (c) 2017 Rishi Raj
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package me.itsrishi.exercisecounter;
